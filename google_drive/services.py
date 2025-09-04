d9ce4bea21d6ab74924a7e299c9da13b5730e36b
sargis petrosyan <saqomax@gmail.com>
2025-09-04 23:04:05 +0200

    set driver client

--- google_drive/services.py
+++ google_drive/services.py
@@ -2,24 +2,22 @@ from datetime import date
 from client import GoogleDriveClient
 
 class GoogleDriveServices:
-    def __init__(self):
+    def __init__(self) -> None:
         self.drive = GoogleDriveClient()
+        self.file_name = "inventory_upload" + str(date.today()).strip()
         
-    def get_drive_file_list(self):
+    def get_drive_files(self) -> list:
         results = (
-            self.drive.client.files()
-            .list(fields="nextPageToken, files(id, name)")
-            .execute()
+            self.
+            drive.
+            client.
+            files().list(fields="nextPageToken, files(id, name)").
+            execute()
         )
-        items = results.get("files", [])
-        
-
-            
-
-
-drive_servies = GoogleDriveServices()
-
-                
-drive_servies.get_drive_file_list()
-
+        print(results.get("files", []))
     
+    def find_drive_file(self,file_name:str):
+        drive_files = self.get_drive_files()
+        
+        for file in drive_files:
+            if file.get["name"] = self.file_name
