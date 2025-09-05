dbdcd61ce68f52cee0456e20df9bf7047c30fe7b
Sargis <sargis.petrosyan.arm@gmail.com>
2025-09-05 21:37:33 +0200

    drive client methods VIP

--- google_drive/services.py
+++ google_drive/services.py
@@ -1,25 +1,30 @@
+import os
+from dotenv import load_dotenv
 from datetime import date
 from client import GoogleDriveClient
+
+load_dotenv()
 
 class GoogleDriveServices:
     def __init__(self):
         self.drive = GoogleDriveClient()
+        self.INVENTORY_FOLDER_ID = os.getenv("INVENTORY_TRACKING_FOLDER_ID")
+        self.INVENTORY_SAMPLE_FILE_ID = os.getenv("INVENTORY_SAMPLE_FILE_ID")
         
-    def get_drive_file_list(self):
-        results = (
-            self.drive.client.files()
-            .list(fields="nextPageToken, files(id, name)")
-            .execute()
-        )
-        items = results.get("files", [])
+    def get_drive_file_list(self) -> list:
+        query = f"'{self.INVENTORY_FOLDER_ID}' in parents and trashed=false"
+        results = self.drive.client.files().list(
+                q=query,
+                pageSize=100,
+                fields="nextPageToken, files(id, name, mimeType)"
+            ).execute()
         
-
-            
-
-
-drive_servies = GoogleDriveServices()
-
-                
-drive_servies.get_drive_file_list()
-
+        return results.get("files", [])
     
+    def duplicate_drive_file(self):
+        self.drive.client().files().copy(
+            fileid=self.INVENTORY_SAMPLE_FILE_ID
+        )
+        
+        
+        
