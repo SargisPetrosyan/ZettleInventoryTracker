54614b79d59d0a15404f1020d038724d1fdc67ba
Sargis <sargis.petrosyan.arm@gmail.com>
2025-09-09 10:39:11 +0200

    set up OAuth credentials for Drive working

--- google_drive/client.py
+++ google_drive/client.py
@@ -3,14 +3,12 @@ import os.path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
 
 def get_drive_credentials():
     creds = None
     # If modifying these scopes, delete the file token.json.
-    SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
+    SCOPES = ["https://www.googleapis.com/auth/drive"]
 
 
     if os.path.exists("token.json"):
@@ -28,12 +26,3 @@ def get_drive_credentials():
             with open("token.json", "w") as token:
                 token.write(creds.to_json())
     return creds
-
-
-class GoogleDriveClient:
-    def __init__(self):
-        creds = get_drive_credentials()
-        try:
-            self.client = build("drive", "v3", credentials=creds)
-        except HttpError as error:
-            raise RuntimeError(f"Failed to build drive client: {error}")
--- google_drive/services.py
+++ google_drive/services.py
@@ -1,30 +1,47 @@
 import os
 from dotenv import load_dotenv
 from datetime import date
-from client import GoogleDriveClient
+from googleapiclient.errors import HttpError
+from googleapiclient.discovery import build
+from client import get_drive_credentials
 
 load_dotenv()
 
 class GoogleDriveServices:
     def __init__(self) -> None:
-        self.drive = GoogleDriveClient()
+        creds = get_drive_credentials()
+        try:
+            self.client = build("drive", "v3", credentials=creds)
+        except HttpError as error:
+            raise RuntimeError(f"Failed to build drive client: {error}")
+        
         self.INVENTORY_FOLDER_ID = os.getenv("INVENTORY_TRACKING_FOLDER_ID")
         self.INVENTORY_SAMPLE_FILE_ID = os.getenv("INVENTORY_SAMPLE_FILE_ID")
+        self.folder_query = f"'{self.INVENTORY_FOLDER_ID}' in parents and trashed=false"
         
     def get_drive_file_list(self) -> list:
-        query = f"'{self.INVENTORY_FOLDER_ID}' in parents and trashed=false"
-        results = self.drive.client.files().list(
-                q=query,
-                pageSize=100,
-                fields="nextPageToken, files(id, name, mimeType)"
-            ).execute()
+        results = self.client.files().list(
+            q=self.folder_query,
+            pageSize=100,
+            fields="nextPageToken, files(id, name)"
+        ).execute()
         
         return results.get("files", [])
     
     def duplicate_drive_file(self):
-        self.drive.client().files().copy(
-            fileid=self.INVENTORY_SAMPLE_FILE_ID
-        )
+        self.client.files().copy(
+            fileId=self.INVENTORY_SAMPLE_FILE_ID
+        ).execute()
+    
+    def delete_drive_file(self):
+        self.client.files().delete(
+            fileId=self.INVENTORY_SAMPLE_FILE_ID
+        ).execute()
         
+    def drive_get_file(self):
+        self.client.files().get(
+            fileId=self.INVENTORY_SAMPLE_FILE_ID,
+            fields="id, name, permissions"
+            ).execute()
         
-        
+    
