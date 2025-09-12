36bd0e3adb4b7b224fd469ea09e420b248a52bf7
Sargis <sargis.petrosyan.arm@gmail.com>
2025-09-12 09:28:45 +0200

    handle sheet data check

--- services/google_drive/sheet_services.py
+++ services/google_drive/sheet_services.py
@@ -3,10 +3,12 @@ import pandas as pd
 from pandas import DataFrame
 
 class DriveSheetServices:
-    def __init__(self) -> None:
+    def __init__(self,product_name: str) -> None:
         self.sheet = GoogleSheetServices()
-        self.row_sheet_data = pd.DataFrame(self.sheet.get_sheet_data())
-        self.sheet_data = self._format_row_data_to_panda()
+        self.product_name:str = product_name
+        self.row_sheet_data:DataFrame = pd.DataFrame(self.sheet.get_sheet_data())
+        self.sheet_data:DataFrame = self._format_row_data_to_panda()
+        self.product_data:DataFrame = self._check_product_exist(product_name)
 
         
     def _format_row_data_to_panda(self) -> DataFrame:
@@ -16,3 +18,14 @@ class DriveSheetServices:
         df = df.reset_index(drop=True)
         
         return df
+
+    def _check_product_exist(self,product_name: str):
+        product_exist:DataFrame = self.sheet_data.loc[self.sheet_data['product_name'] == product_name]
+        
+        if product_exist.empty:
+            return self.create_new_object()
+        return product_exist
+    
+    def create_new_object(self) ->DataFrame:
+        return 'new product was created'
+    
