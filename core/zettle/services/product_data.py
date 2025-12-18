71ac4bbf88781b5c65b019f7a6198bd5b8e2fd75
Sargis <saqomax@gmail.com>
2025-12-18 11:29:12 +0100

    product service get product data

---
+++ core/zettle/services/product_data.py
@@ -0,0 +1,19 @@
+import httpx
+import rich
+from core.zettle.auth import ZettleCredentialsManager
+
+
+class ProductService:
+    def __init__(self, shop_name:str,) -> None:
+        self.creds_manager: ZettleCredentialsManager = ZettleCredentialsManager(shop_name=shop_name)
+
+    def get_product_data(self,product_uuid:str,organization_uuid:str) -> dict:
+        access_token: str = self.creds_manager.get_access_token()
+        result: httpx.Response = httpx.get(
+        url=f'https://products.izettle.com/organizations/{organization_uuid}/products/{product_uuid}',
+        headers={
+            'Authorization': f'Bearer {access_token}',
+        })
+        return result.json()
+
+    
