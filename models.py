39945f9b001bf543e11d832f143c45ca22ccec66
Sargis <saqomax@gmail.com>
2025-12-19 17:02:11 +0100

    model fields creation

--- models.py
+++ models.py
@@ -1,11 +1,14 @@
-from sqlmodel import Field, SQLModel
+import datetime
+from sqlmodel import Field, SQLModel, DateTime
+import uuid
+
 
 
 class InventoryBalanceUpdate(SQLModel, table=True):
-    shop_id: int = Field(default=None, primary_key=True)
-    timestamp: str
-    product_id: str
-    variant_id:str
+    shop_id:uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    timestamp:datetime.datetime = Field(default_factory=DateTime)
+    product_id:uuid.UUID  = Field(default_factory=uuid.uuid1)
+    variant_id:uuid.UUID = Field(default_factory=uuid.uuid1)
     category: str | None = None
     name: str
     before:int
@@ -14,6 +17,7 @@ class InventoryBalanceUpdate(SQLModel, table=True):
 
     def __repr__(self) -> str:
         return f"""<InventoryBalanceUpdate(name='{self.name}', 
+        timestamp='{self.timestamp}
         name='{self.name}, 
         before:{self.before}, 
         after:{self.after}, 
