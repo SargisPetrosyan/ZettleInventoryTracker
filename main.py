1add27075544c390d3ed98ed70e0167cb2054f4b
Sargis <saqomax@gmail.com>
2025-12-22 21:15:27 +0100

    subscription handler with inventory update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from operator import inv
+from ast import Dict
+from blinker import ANY
 from fastapi import FastAPI, Request
-from flask import request
-import rich
-from core.utils import OrganizationsNameMappedId
 import logging
+
+import rich
 from core.zettle.validation.inventory_update_validation import InventoryBalanceUpdateValidation
 from logging_config import setup_logger
 from setup_db import Database
@@ -17,9 +17,11 @@ webhook_handler = SubscriptionHandler()
 
 app = FastAPI()
 
-@app.post("/store_inventory_data_webhook")
-async def store_inventory_data_webhook(request: Request) -> None:
-    payload = await request.json() 
+@app.post(path="/store_inventory_data_webhook")
+async def store_inventory_data_webhook(request: Request) -> None | dict:
+    if request.headers["user-agent"] == "PusherApplication (WebHookPushClient)": # need to change 
+        return {"status":"202"}
+    payload:dict = await request.json() 
     validated_data = InventoryBalanceUpdateValidation(**payload)
     webhook_handler.process_subscription(inventory_update=validated_data, database=database)
 
